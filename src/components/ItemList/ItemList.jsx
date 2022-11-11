5ac724af6a8569c14ec45b66f9a6ef324a915223
francoariel1492 <francoariel1492@gmail.com>
2022-11-11 19:46:25 -0300

    PreEntrega2+Parente

---
+++ src/components/ItemList/ItemList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Item from '../Item/Item'
+
+const ItemList = ({products}) => {
+  return (
+
+      <div className="d-flex flex-row flex-wrap justify-content-center">
+        {products.map((obj) => (
+          <Item
+            obj={obj}
+            key={obj.id}
+          />
+        ))}
+      </div>
+  )
+}
+
+export default ItemList
