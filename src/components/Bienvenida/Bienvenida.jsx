f3d079c31d0f13d1f0b544522698d9318d3c45f1
francoariel1492 <francoariel1492@gmail.com>
2022-11-30 00:30:54 -0300

    Entrega Proyecto Final

--- src/components/Bienvenida/Bienvenida.jsx
+++ src/components/Bienvenida/Bienvenida.jsx
@@ -2,7 +2,7 @@ const Bienvenida = () => {
   return (
     <div className="text-center bg-dark text-light">
       <p className="p-2">
-        Gracias por visitarnos! Esperamos que encuentres lo que buscas
+        Gracias por visitarnos! Esperamos que encuentres lo que buscas.
       </p>
     </div>
   );
